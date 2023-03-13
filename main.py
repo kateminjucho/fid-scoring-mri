9f78a6a210fa145758e54b345bd433a964f0e648
skyc5423 <skyc5423@kaist.ac.kr>
2023-03-13 21:29:47 +0900

    added inceptionnet importing

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 '''
 Created by Minjuc on Mar.13.2023
 '''
+import torch
+
 
 def main():
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
     print('Hello world')
 
 
