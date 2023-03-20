6a147b8b57437f269b82bbbd9bb8df4de77e1bc4
skyc5423 <skyc5423@kaist.ac.kr>
2023-03-20 21:34:39 +0900

    added cosine similarity and modified main to calculate distance for all data

--- distances.py
+++ distances.py
@@ -9,8 +9,19 @@ Reference
 ...
 '''
 import numpy as np
+from scipy.spatial import distance
 
 
 class EuclideanDistance:
     def __call__(self, x, y):
         return np.sqrt(np.sum(np.square(x - y)))
+
+
+class CosineSimilarity:
+    def __call__(self, x, y):
+        return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+
+class MahalanobisNorm:
+    def __call__(self, x, y):
+        return distance.mahalanobis(x, y)
--- main.py
+++ main.py
@@ -7,38 +7,44 @@ import timm
 from utils import img_to_array, norm_dcm_array, preprocess
 import glob
 import numpy as np
-from distances import EuclideanDistance
+from distances import EuclideanDistance, CosineSimilarity
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def main():
-    standard_path = sorted(glob.glob("./AM002_20220812_3691639/standard/5_T1 AX FSE/*.dcm"))
-    swift_path = sorted(glob.glob("./AM002_20220812_3691639/swift/6_T1 AX FSE_swift/*.dcm"))
+    standard_path = sorted(glob.glob("./AM002_20220812_3691639/standard/*/*.dcm"))
+    swift_path = sorted(glob.glob("./AM002_20220812_3691639/swift/*/*.dcm"))
     # swift_recon_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon/6_T1 AX FSE_swift/*.dcm"))
-    swift_recon_low_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_low/6_T1 AX FSE_swift/*.dcm"))
-    swift_recon_medium_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_medium/6_T1 AX FSE_swift/*.dcm"))
-    standard_img = Image.fromarray(norm_dcm_array(img_to_array(standard_path[0]))).convert("RGB")
-    swift_img = Image.fromarray(norm_dcm_array(img_to_array(swift_path[0]))).convert("RGB")
-    swift_recon_low_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_low_path[0]))).convert("RGB")
-    swift_recon_medium_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_medium_path[0]))).convert("RGB")
+    swift_recon_low_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_low/*/*.dcm"))
+    swift_recon_medium_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_medium/*/*.dcm"))
+    sim_matrix = np.zeros([4, 4])  # * len(standard_path)
 
-    file_path_list = [standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img]
-    img_list: list = [preprocess(fp) for fp in file_path_list]
-
-    distance = EuclideanDistance()
-
+    distance = CosineSimilarity()
     m = timm.create_model('inception_v3', pretrained=True, num_classes=0)
-    feature_list: list = [m(img.unsqueeze(0)).squeeze(0).detach().numpy() for img in img_list]
-    sim_matrix = np.zeros([len(feature_list), len(feature_list)])
-    for i in range(len(feature_list)):
-        for j in range(i + 1, len(feature_list)):
-            distance_tmp = distance(feature_list[i], feature_list[j])
-            sim_matrix[i, j] = distance_tmp
-            sim_matrix[j, i] = distance_tmp
 
+    for n in tqdm(range(len(standard_path))):
+        standard_img = Image.fromarray(norm_dcm_array(img_to_array(standard_path[n]))).convert("RGB")
+        swift_img = Image.fromarray(norm_dcm_array(img_to_array(swift_path[n]))).convert("RGB")
+        swift_recon_low_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_low_path[n]))).convert("RGB")
+        swift_recon_medium_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_medium_path[n]))).convert(
+            "RGB")
+
+        file_path_list = [standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img]
+        img_list: list = [preprocess(fp) for fp in file_path_list]
+
+        feature_list: list = [m(img.unsqueeze(0)).squeeze(0).detach().numpy() for img in img_list]
+        for i in range(len(feature_list)):
+            for j in range(i, len(feature_list)):
+                distance_tmp = distance(feature_list[i], feature_list[j])
+                sim_matrix[i, j] += distance_tmp
+                if i != j:
+                    sim_matrix[j, i] += distance_tmp
+
+    sim_matrix = sim_matrix / len(standard_path)
     fig = plt.figure()
     plt.imshow(sim_matrix)
-    fig.savefig('./sim_matrix.png')
+    fig.savefig('./cosine_sim_matrix.png')
 
 
 if __name__ == '__main__':
