8e65ec7e501e6a959f0e70098192b651edaa7502
skyc5423 <skyc5423@kaist.ac.kr>
2023-04-04 00:48:33 +0900

    added recent

--- main.py
+++ main.py
@@ -59,69 +59,91 @@ def feature_computation(m, distance, standard_path, swift_path, swift_recon_low_
     fig.savefig('./eud_sim_matrix_512.png')
 
 
-def grad_cam(m, standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img):
+def grad_cam(m, standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img, level: int = 10):
     file_path_list = [standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img]
     # Preprocess 해서 list 형태로 만들어 둠.
     img_list: list = [preprocess(fp) for fp in file_path_list]
 
-    # 모델을 두 부분으로 나눔 (M_BEFORE, M_AFTER)
-    m_before = torch.nn.Sequential(*list(m.children())[:10])
-    m_after = torch.nn.Sequential(*list(m.children())[10:])
+    # 모델을 두 부분으로 나눔 (m_before, m_after). 숫자는 layer index
+    m_before = torch.nn.Sequential(*list(m.children())[:level])
+    m_after = torch.nn.Sequential(*list(m.children())[level:])
+
+    # m_before을 forwarding 해서 feature 뽑음. 이 feature에 대해서 gradCAM 적용
     with torch.no_grad():
         img1 = img_list[0].unsqueeze(0)
         img2 = img_list[2].unsqueeze(0)
         feat1 = m_before(img1)
         feat2 = m_before(img2)
 
+    # feature 2에 대해서는 gradient 계산해야 함.
     feat2.requires_grad = True
 
+    # m_after을 forwarding 해서 최종 feature 뽑음. 이 feature로 두 이미지의 차이를 구함.
     out1 = m_after(feat1)
     out2 = m_after(feat2)
     # diff = torch.mean(d[0, d_idx[0, -10:]])
 
-    diff = F.mse_loss(out1, out2, reduction='sum')
-    # diff = F.cosine_similarity(out1, out2)
+    # feature 차이 구함.
+    # indices = torch.square(out1 - out2).argsort(dim=1)[0][-10:]
+    diff = F.mse_loss(out1[:, :], out2[:, :], reduction='sum')
 
+    # backpropagation 해서 gradient 계산하기.
     diff.backward(retain_graph=True)
 
+    # feat2에 저장되어 있는 gradient 가져오기
     grads = feat2.grad
 
+    # 각 channel 방향의 feature에 대해서 중요도 계산 (width, height 평균)
     weights = torch.mean(grads, axis=(2, 3), keepdim=True)
-    # weights = grads
 
-    cam = torch.abs((weights * feat2).sum(axis=1, keepdim=True)).detach().numpy()[0, 0]
-    # cam = weights.sum(axis=1, keepdim=True).detach().numpy()[0, 0]
-    # Normalize the gradients
-    cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam) + 1E-9)
+    # Weight * feature 해서 gradcam 계산
+    cam = torch.abs((weights * feat2).mean(axis=1, keepdim=True)).detach().numpy()[0, 0]
+    # Normalize the gradcam
+    cam_norm = 1 - ((cam - np.min(cam)) / (np.max(cam) - np.min(cam) + 1E-9))
 
-    red_grads = (255 * cam).astype(np.uint8)
+    # 결과 저장
+    red_grads = (255 * cam_norm).astype(np.uint8)
     heatmap = Image.fromarray(cv2.applyColorMap(red_grads, cv2.COLORMAP_JET)).resize((512, 512),
                                                                                      resample=Image.Resampling.BICUBIC)
     out_img = Image.new('RGB', (512 * 3, 512))
     out_img.paste(swift_recon_low_img, (0, 0))
     out_img.paste(Image.blend(swift_recon_low_img, heatmap, 0.5), (512, 0))
     out_img.paste(standard_img, (512 * 2, 0))
-    return out_img
+    return out_img, cam_norm
 
 
 def main():
     standard_path = sorted(glob.glob("./AM002_20220812_3691639/standard/*/*.dcm"))
     swift_path = sorted(glob.glob("./AM002_20220812_3691639/swift/*/*.dcm"))
-    # swift_recon_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon/6_T1 AX FSE_swift/*.dcm"))
     swift_recon_low_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_low/*/*.dcm"))
     swift_recon_medium_path = sorted(glob.glob("./AM002_20220812_3691639/swift_recon_medium/*/*.dcm"))
-    sim_list = []
 
     m = timm.create_model('inception_v3', pretrained=True, num_classes=0)
-    for n in range(50):
-        standard_img = Image.fromarray(norm_dcm_array(img_to_array(standard_path[n]))).convert("RGB")
-        swift_img = Image.fromarray(norm_dcm_array(img_to_array(swift_path[n]))).convert("RGB")
-        swift_recon_low_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_low_path[n]))).convert("RGB")
-        swift_recon_medium_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_medium_path[n]))).convert(
-            "RGB")
+    for n in range(20, 110):
+        grads_gathered = np.zeros([512, 512]).astype(np.float32)
+        for level in range(0, 19):
+            standard_img = Image.fromarray(norm_dcm_array(img_to_array(standard_path[n]))).convert("RGB")
+            swift_img = Image.fromarray(norm_dcm_array(img_to_array(swift_path[n]))).convert("RGB")
+            swift_recon_low_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_low_path[n]))).convert("RGB")
+            swift_recon_medium_img = Image.fromarray(norm_dcm_array(img_to_array(swift_recon_medium_path[n]))).convert(
+                "RGB")
 
-        grad_cam(m, standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img).save(
-            './mse_17_%02d-feat.png' % (n))
+            grad_cam_img, grads = grad_cam(m, standard_img, swift_img, swift_recon_low_img, swift_recon_medium_img,
+                                           level)
+            grads = cv2.resize(grads, (512, 512), interpolation=cv2.INTER_CUBIC)
+            grads_gathered += grads
+
+        grads_gathered_norm = 255 * (grads_gathered - grads_gathered.min()) / (grads_gathered.max() - grads_gathered.min())
+        heatmap = Image.fromarray(cv2.applyColorMap(grads_gathered_norm.astype(np.uint8), cv2.COLORMAP_JET))
+
+        diff = (np.array(standard_img).astype(np.float32) - np.array(swift_recon_low_img).astype(np.float32) + 255) / 2
+        diff_img = Image.fromarray(cv2.applyColorMap(diff.astype(np.uint8), cv2.COLORMAP_JET))
+        final_img = Image.new('RGB', (512 * 4, 512))
+        final_img.paste(swift_recon_low_img, (0, 0))
+        final_img.paste(Image.blend(swift_recon_low_img, heatmap, 0.5), (512, 0))
+        final_img.paste(standard_img, (512 * 2, 0))
+        final_img.paste(diff_img, (512 * 3, 0))
+        final_img.save('./result/final_%02d.png' % n)
 
 
 if __name__ == '__main__':
